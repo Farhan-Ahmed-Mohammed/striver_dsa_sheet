c2519fab92cde85af44e393ef7cd11df16ea1d80
Farhan-Ahmed-Mohammed <farhan.ou888@gmail.com>
2025-05-23 18:31:30 +0530

    Create search in rotated sorted array 1.cpp

---
+++ search in rotated sorted array 1.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    int searchPivot(vector<int>& nums)  // pivot is smallest element 
+    {
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        int mid=(low+high)/2;
+        while(low<high)
+        {
+            
+            if(nums[mid]>=nums[0])
+            {
+                low=mid+1;
+            }
+
+            else
+            {
+                high=mid;
+            }
+            mid=(low+high)/2;
+        }
+        return mid;
+        
+    }
+    int binarySearch(vector<int>& nums,int target,int low,int high)
+    {
+        int mid=(low+high)/2;
+        while(low<=high)
+        {
+            mid=(low+high)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+
+            else if(nums[mid]>target)
+            {
+                high=mid-1;
+            }
+
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        int mid=(low+high)/2;
+        int pivot=searchPivot(nums);
+            
+        
+            
+            if(nums[pivot]<=target  && target<=nums[n-1])
+            {
+                return binarySearch(nums,target,pivot,high);
+            }
+
+            else
+            {
+                return binarySearch(nums,target,low,pivot-1);
+            }
+           
+        
+        return -1;
+        
+    }
+};
