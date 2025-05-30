f4d190809a3707ca945bfe96a81fc36e3b2d8b7c
Farhan-Ahmed-Mohammed <farhan.ou888@gmail.com>
2025-05-30 21:55:07 +0530

    Update kth missing positive integer.cpp

--- kth missing positive integer.cpp
+++ kth missing positive integer.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {  //brute force solution below is b.s soluiton
 public:
     int findKthPositive(vector<int>& arr, int k) {
         vector<int> mis;
@@ -52,6 +52,33 @@ public:
 
        
         return ans;
+        
+    }
+};
+
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n=arr.size();
+        int low=0;
+        int high=n-1;
+      
+        while(low<=high)
+        {
+             int mid=low+(high-low)/2;
+             if(arr[mid]<=mid+k)
+             {
+                low=mid+1;
+             }
+
+             else
+             {
+                high=mid-1;
+             }
+        }
+        return low+k;
+
         
     }
 };
