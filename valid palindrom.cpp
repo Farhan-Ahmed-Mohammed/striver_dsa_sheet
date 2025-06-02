e5c802d6141768acb7ec80b06bdc4ebf56adef1c
Farhan-Ahmed-Mohammed <farhan.ou888@gmail.com>
2025-06-02 11:45:40 +0530

    Create valid palindrom.cpp

---
+++ valid palindrom.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str;
+
+        // Filter and convert to lowercase
+        for (int i=0;i<s.length();i++) {
+            char ch=s[i];
+            if (ch >= 'A' && ch <= 'Z') {
+                str.push_back(tolower(ch));
+            }
+            else if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')) {
+                str.push_back(ch);
+            }
+        }
+
+        int i = 0, j = str.size() - 1;
+        while (i <= j) {
+            if (str[i] != str[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
