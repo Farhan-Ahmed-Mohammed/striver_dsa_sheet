83511fcec598caf5d1cff8cbfbd86f8578d8fbf1
Farhan-Ahmed-Mohammed <farhan.ou888@gmail.com>
2025-06-08 21:06:01 +0530

    Create divide two intergers.cpp

---
+++ divide two intergers.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+   
+    int divide(int dividend, int divisor) {
+        long long  a=dividend;
+        long long  b=divisor;
+        long long ans=a/b;
+        return ans;
+        
+    }
+};
