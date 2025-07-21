c1974b070f6d7d2fd2227ebcf5cba4cc66dde8fe
Farhan-Ahmed-Mohammed <farhan.ou888@gmail.com>
2025-07-21 10:01:29 +0530

    Update sevie of enthosis.cpp

--- sevie of enthosis.cpp
+++ sevie of enthosis.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<bool> check(n+1,true); //first we assume all to be prime except 1 and 0 then we eliminate all the multiples of 2 from prime list 
+        vector<bool> check(n+1,true); //first we assume all to be prime we are creating a vector of size n+1 and all have true in its position from index 0 to nexcept 1 and 0 then we eliminate all the multiples of 2 from prime list 
         int cnt=0;
         check[0]=check[1]=false;
         for(int i=2;i<n;i++)
