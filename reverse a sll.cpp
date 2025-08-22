3dff8b49ae79f32238296cd9a107c6888af3f749
Farhan-Ahmed-Mohammed <farhan.ou888@gmail.com>
2025-08-22 15:58:21 +0530

    Create reverse a sll.cpp

---
+++ reverse a sll.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* temp=head;
+        ListNode* prev=NULL;
+        ListNode* fard=NULL;
+
+        while(temp!=NULL)
+        {
+            fard=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=fard;
+        }
+
+        return prev;
+
+        
+    }
+};
