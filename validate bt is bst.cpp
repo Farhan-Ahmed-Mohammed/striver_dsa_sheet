c62638d95a9a7473d6ad36741c13a7ade66b1380
Farhan-Ahmed-Mohammed <farhan.ou888@gmail.com>
2025-09-15 22:36:47 +0530

    Create validate bt is bst.cpp

---
+++ validate bt is bst.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void in(TreeNode* temp,vector<int> &ans)
+    {
+        if(temp==NULL)
+        {
+            return;
+        }
+
+        in(temp->left,ans);
+        if(temp!=NULL)
+        {
+            ans.push_back(temp->val);
+        }
+        in(temp->right,ans);
+    }
+
+    bool isValidBST(TreeNode* root) {  //we can make it more optimised just stroe the previousn in some consatnt and compare evenery time in the inorder function it pre is grater then ans=false or else ans is true
+        TreeNode* temp=root;
+        vector<int> ans;
+        in(temp,ans);
+        for(int i=0;i<ans.size()-1;i++)
+        {
+            if(ans[i]>=ans[i+1])
+            {
+                return false;   //check that they are in increasing order if not then we can return false
+            }
+        }
+       
+        return true;
+    }
+};
