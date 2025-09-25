4a00ca22a2f78d6d01a905ac27a31e93e925bbee
Farhan-Ahmed-Mohammed <farhan.ou888@gmail.com>
2025-09-25 23:19:51 +0530

    Create greedy cookie assign.cpp

---
+++ greedy cookie assign.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int cnt=0;
+        int i=0;
+        int j=0;
+        while(i<g.size() && j<s.size())
+        {
+            if(s[j]>=g[i])
+            {
+                cnt++;
+                i++;
+                j++;
+            }
+
+            else
+            {
+                j++;  // it means move to next bigger cookie
+            }
+        }
+        return cnt;
+        
+    }
+};
