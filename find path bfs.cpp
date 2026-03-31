8420fa7070526509bf4165f1b2629e397a0a60de
Farhan-Ahmed-Mohammed <farhan.ou888@gmail.com>
2026-03-31 16:54:55 +0530

    Add BFS solution for valid path in graph
    Implement BFS to check if a valid path exists between source and destination.

---
+++ find path bfs.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        unordered_map<int,vector<int>> adj;
+
+        for(int i=0;i<edges.size();i++)
+        {
+            int u=edges[i][0];
+            int v=edges[i][1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        queue<int> q;
+        vector<int> vis(n,0);
+        q.push(source);
+        vis[source]=1;
+
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+
+            if(node==destination)
+            {
+                return true;
+            }
+
+            for(int i=0;i<adj[node].size();i++)
+            {
+                int nigh=adj[node][i];
+
+                if(!vis[nigh])
+                {
+                    vis[nigh]=1;
+                    q.push(nigh);
+                }
+            }
+        }
+        return false;
+        
+
+    }
+};
